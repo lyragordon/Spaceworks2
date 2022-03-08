c7d9c8b3edb5acef7db8ba60b11d382c9696ba09
Lyra Gordon <lyragordon@asu.edu>
2022-03-07 18:36:55 -0800

    removed centerWindow, implemented exit warning

--- Spaceworks2/gui.py
+++ Spaceworks2/gui.py
@@ -3,6 +3,7 @@ import comm
 import time
 import dummy_serial
 from serial import Serial
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QThread, QObject, pyqtSignal
@@ -29,6 +30,7 @@ class MainWindow(QMainWindow):
     def __init__(self,parent=None):
         super().__init__(parent=parent)
         # window settings
+        self.setWindowTitle("SW2")
         self.setWindowIcon(self.style().standardIcon(getattr(QStyle,'SP_CommandLink')))
         self.serial = None
         # prompt for serial config
@@ -55,7 +57,7 @@ class MainWindow(QMainWindow):
         self.window.show()
         # window settings
         self.setCentralWidget(self.window)
-        self.centerWindow()
+        
         self.show()
         
     def update_terminal(self, line:str):
@@ -106,13 +108,19 @@ class MainWindow(QMainWindow):
         self.terminal_worker.progress.connect(self.update_terminal)
         self.terminal_thread.start()
 
-    def centerWindow(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
 
-        
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        if self.serial:
+            reply = QMessageBox.question(self,"Exit?","A serial connection is active. Do you really want to exit?",QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                event.accept()
+                return super().closeEvent(event)
+            else:
+                event.ignore()
+        else:
+            event.accept()
+    
+   
     
     
 
@@ -152,7 +160,6 @@ class SerialSetup(QDialog):
         self.setLayout(self.horiz_layout)
         # window settings
         self.resize(self.sizeHint())
-        self.centerWindow()
         self.show()
 
     def evt_btn_Ok(self):
@@ -197,9 +204,3 @@ class SerialSetup(QDialog):
             self.cbb_Baudrate.addItems(new_options)
         if saved_selection in new_options:
             self.cbb_Baudrate.setCurrentText(saved_selection)
-
-    def centerWindow(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
